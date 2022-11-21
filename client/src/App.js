3f30067973b767c4c0222b34b957e02d5b5b54eb
Parth Kapadia <parthkapadia5565@gmail.com>
2022-11-21 22:02:25 +0530

    add front-end

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+
+function App() {
+  return (
+    <div>
+      <p>parth kapadia</p>
+    </div>
+  );
+}
+
+export default App;
